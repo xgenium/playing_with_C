880affcae4a3ebd1ddef31d62697998ad6099198
realkolobok <kolobokkitajskij@gmail.com>
2025-11-28 22:13:35 +0200

    I dont remember

--- playing_with_dlopen/main.c
+++ playing_with_dlopen/main.c
@@ -6,6 +6,7 @@ int main()
   void *lib = dlopen("raylib-5.5_linux_amd64/lib/libraylib.so", RTLD_NOW);
   printf("lib = %p\n", lib);
   void (*InitWindow)(int width, int height, const char *title) = dlsym(lib, "InitWindow");
+  void (*WindowShouldClose)(void) = dlsym(lib, "WindowShouldClose");
   printf("InitWindow = %p\n", InitWindow);
   InitWindow(400, 400, "My Window");
 
